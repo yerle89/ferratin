8a01f3b2b650ac7cedc2bcd31e83134e5295003c
yerle89 <41743082+yerle89@users.noreply.github.com>
2020-12-20 21:46:08 +0100

    Seminario 2. Ahora cart_pr

--- Seminario2/src/carrito_module.js
+++ Seminario2/src/carrito_module.js
@@ -1,6 +1,6 @@
 const products_db = require('./database');
-let available_products = [];
-const cart_products = {};
+let available_products = new Map();
+const cart_products = new Map();
 /* ejemplo de cart = [{
     id: 1,
     name: "iPhone",
@@ -9,37 +9,42 @@ const cart_products = {};
 
 exports.get_available_products = async function () {
     const products = await products_db.get_products();
-    available_products = products;
+    products.forEach(product => {
+        available_products.set(product._id, product);
+    });
     return products;
 }
 
 exports.get_cart_products = function () {
-    return cart_products;
+    let res_cart_products = [];
+    for (let [key, value] of cart_products) {
+        res_cart_products.push(value);
+    }
+    return res_cart_products;
 }
 
 exports.add_product_to_cart_by_id = async function (_id) {
     if (await products_db.check_available_product_by_id(_id)) {
-        if (cart_products[_id] === undefined) {
-            let new_product = available_products.find(x => x._id === _id)
-            cart_products[_id] = {name: new_product.name, units: 1}
-            return 'Producto añadido con éxito'
+        if (cart_products.get(_id) === undefined) {
+            let new_product = available_products.get(_id);
+            //Hacer consulta sobre la base de datos y comprobar stock
+            cart_products.set(_id, { name: new_product.name, units: 1 });
         } else {
-            cart_products[_id].units++
-            return 'Producto añadido con éxito'
+            cart_products.get(_id).units++;
         }
+        return 'Producto añadido con éxito';
     }
     return 'Producto no añadido porque no hay stock';
 }
 
 exports.remove_product_from_cart_by_id = function (_id) {
-    if (cart_products[_id]) {
-        if (cart_products[_id].units == 1) {
-            delete cart_products[_id]
-            return 'Producto eliminado con éxito'
+    if (cart_products.get(_id)) {
+        if (cart_products.get(_id).units == 1) {
+            cart_products.delete(_id);
         } else {
-            cart_products[_id].units--
-            return 'Producto eliminado con éxito'
+            cart_products.get(_id).units--;
         }
+        return 'Producto eliminado con éxito';
     }
     return 'No existe este producto en el carrito!';
 }
