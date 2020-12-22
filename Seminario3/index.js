718b01ad18722589e63c60efade4ea41ac107c7c
Ferran <ferranponsuniversidad@gmail.com>
2020-12-22 16:48:25 +0100

    chat funcionando. falta retocar la interfaz de usuario

--- Seminario3/index.js
+++ Seminario3/index.js
@@ -1,32 +1,70 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
+const connected_users = new Map();
+const typing_users = new Map();
 
-// settings
-app.set('port', process.env.PORT || 3000);
+function get_connected_users() {
+  const res_connected_users = [];
+  for (const [socket_id, nickname] of connected_users) {
+    res_connected_users.push({
+      id: socket_id,
+      nickname: nickname
+    });
+  }
+  return res_connected_users;
+}
 
-// static files
+function get_typing_users() {
+  const res_typing_users = [];
+  for (const [socket_id, nickname] of typing_users) {
+    res_typing_users.push({
+      id: socket_id,
+      nickname: nickname
+    });
+  }
+  return res_typing_users;
+}
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// start server
-const server = app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
+io.on('connection', socket => {
+
+  socket.on('on_connected_user', data => {
+    connected_users.set(socket.id, data.nickname);
+    const res_connected_users = get_connected_users();
+    io.sockets.emit('connected_users', res_connected_users);
+    const res_typing_users = get_typing_users();
+    io.sockets.emit('typing_users', res_typing_users);
+  });
+
+  socket.on('disconnect', () => {
+    connected_users.delete(socket.id);
+    const res_connected_users = get_connected_users();
+    socket.broadcast.emit('connected_users', res_connected_users);
+  });
+
+  socket.on('send_text_message_to_server', data => {
+    socket.broadcast.emit('broadcast_text_message', data);
+  });
+
+  socket.on('on_start_typing', data => {
+    typing_users.set(socket.id, data.nickname);
+    const res_typing_users = get_typing_users();
+    io.sockets.emit('typing_users', res_typing_users);
+  });
+
+  socket.on('on_finish_typing', () => {
+    typing_users.delete(socket.id);
+    const res_typing_users = get_typing_users();
+    io.sockets.emit('typing_users', res_typing_users);
+  });
+
 });
 
-// websockets
-const SocketIO = require('socket.io');
-const io = SocketIO(server);
-
-io.on('connection', (socket) => {
-    console.log('new connection', socket.id);
-
-    socket.on('chat:message', (data) => {
-        io.sockets.emit('for_all', data);
-    });
-
-    socket.on('chat:typing', (username) => {
-        socket.broadcast.emit('broadcast', username);
-    });
+http.listen(3000, function () {
+  console.log('listening on *:3000');
 });
-
--- Seminario3/public/chat.js
+++ Seminario3/public/chat.js
@@ -1,32 +1,92 @@
-const socket = io();
+let socket;
+let typing = false;
 
+// nickname DOM elements
+const div_nickname = document.getElementById('div_nickname');
+const nickname_form = document.getElementById('nickname_form');
+const nickname_login = document.getElementById('nickname_login');
 
-// DOM elements
-let message = document.getElementById('message');
-let username = document.getElementById('username');
-let btn = document.getElementById('send');
-let output = document.getElementById('output');
-let actions = document.getElementById('actions');
+// chat DOM elements
+const div_chat = document.getElementById('div_chat');
+const connected_users = document.getElementById('connected_users');
+const messages = document.getElementById('messages');
+const text_message = document.getElementById('text_message');
+const nickname_logged = document.getElementById('nickname_logged');
+const text_message_form = document.getElementById('text_message_form');
+const typing_users = document.getElementById('typing_users');
 
-btn.addEventListener('click', function () {
-    socket.emit('chat:message', {
-        message: message.value,
-        username: username.value
+nickname_form.addEventListener('submit', e => {
+    e.preventDefault();
+    socket = io();
+
+    div_nickname.style.display = 'none';
+    div_chat.style.display = 'block';
+    nickname_logged.value = nickname_login.value;
+
+    socket.emit('on_connected_user', {
+        nickname: nickname_login.value
+    });
+
+    socket.on('connected_users', data => {
+        let res_connected_users = '';
+        for (const user of data) {
+            if (user.id != socket.id) {
+                if (res_connected_users === '')
+                    res_connected_users = user.nickname;
+                else
+                    res_connected_users = res_connected_users.concat(' ', user.nickname);
+            }
+        }
+        connected_users.innerHTML = res_connected_users;
+    });
+
+    socket.on('typing_users', data => {
+        let num_typing_users = 0;
+        let res_typing_users = '';
+        for (const user of data) {
+            if (user.id != socket.id) {
+                if (res_typing_users === '')
+                    res_typing_users = user.nickname;
+                else
+                    res_typing_users = res_typing_users.concat(', ', user.nickname);
+                num_typing_users++;
+            }
+        }
+        if (num_typing_users === 0)
+            typing_users.innerHTML = '';
+        else if (num_typing_users === 1)
+            typing_users.innerHTML = res_typing_users + ' is typing';
+        else
+            typing_users.innerHTML = res_typing_users + ' are typing';
+    });
+
+    socket.on('broadcast_text_message', data => {
+        messages.innerHTML += `<li>
+        <strong>${data.nickname}</strong>: ${data.message}
+        </li>`;
+    });
+
+});
+
+text_message_form.addEventListener('submit', e => {
+    e.preventDefault();
+    socket.emit('send_text_message_to_server', {
+        nickname: nickname_logged.value,
+        message: text_message.value
     });
 });
 
-message.addEventListener('keypress', function () {
-    socket.emit('chat:typing', username.value);
-})
-
-socket.on('for_all', (data) => {
-    output.innerHTML += `<p>
-    <strong>${data.username}</strong>: ${data.message}
-    </p>`;
-});
-
-socket.on('broadcast', (username) => {
-    actions.innerHTML = `<p>
-    <em> ${username} is typing a message</em>
-    </p>`;
+text_message.addEventListener('keyup', () => {
+    if (text_message.value !== '') {
+        if (!typing) {
+            socket.emit('on_start_typing', {
+                nickname: nickname_logged.value
+            });
+        }
+    } else {
+        typing = false;
+        socket.emit('on_finish_typing', {
+            nickname: nickname_logged.value
+        });
+    }
 });
